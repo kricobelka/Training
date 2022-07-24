b1474cbebfbe89a56ae55e1a56df029340f04f37
Hanna Ivanova <hijikataanya@gmail.com>
2022-07-25 00:49:20 +0300

    Min index of the max element

--- Task1/Task2minindex/Program.cs
+++ Task1/Task2minindex/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Linq;
 
-namespace Task1
+namespace Task2minindex
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please insert number of elements");
+            
+            Console.WriteLine("Number of elements");
             int N = int.Parse(Console.ReadLine());
             int[] array1 = new int[N];
             for (int el = 0; el < array1.Length; el++)
@@ -15,29 +15,19 @@ namespace Task1
                 Console.WriteLine("Please insert elements of the array");
                 array1[el] = int.Parse(Console.ReadLine());
             }
-            //output of all the elements from the array:
-            //foreach (int el in array1)
-            //{
-            //    Console.WriteLine(el);
-            //}
-
-            //first option to output max value:
             int max2 = 0;
+            int j = 0;
             for (int i = 0; i < array1.Length; i++)
             {
                 if (max2 <= array1[i])
                 {
                     max2 = array1[i];
+                    j = i;
                 }
             }
-            //Console.WriteLine(max2);
-
-            ////secod option:
-            ////int max = array1.Max();
-            //Console.WriteLine(max);
-            Console.WriteLine("Max element is:" + " " + max2);
-                Console.ReadKey();
-            }
+            Console.WriteLine("Max element is: " + " " + max2);
+            Console.WriteLine("Min index of the max element is" + " " + j);
+            Console.ReadKey();
         }
     }
-
+}
